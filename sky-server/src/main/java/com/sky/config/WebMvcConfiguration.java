46ea23f078b1282e2c4b7b28408bdfdf93014cd8
charcoal_fire <S100286109@163.com>
2025-07-08 15:20:45 +0800

    修复消息转换器与Knife4j的冲突

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -72,6 +72,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         //设置对象转换器，底层使用Jackson将Java对象转为json
         messageConverter.setObjectMapper(new JacksonObjectMapper());
         //将上面的消息转换器追加到mvc框架的转换器集合中
-        converters.add(0, messageConverter);
+        converters.add(1, messageConverter);
     }
 }
